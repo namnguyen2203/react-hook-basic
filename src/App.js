1d0ec1bb238d3ceba351bc90ff93c0ba97348958
Nam Nguyen <namnguyen220302000@gmail.com>
2020-04-09 20:54:06 +0700

    Add Pagination Component ⚙️

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
 import React, { useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import queryString from 'query-string'
 
 import ColorBox from './components/ColorBox'
 import TodoList from './components/TodoList'
 import TodoForm from './components/TodoForm'
 import PostList from './components/PostList'
+import Pagination from './components/Pagination'
 import './App.scss'
 
 function App() {
   const [todoList, setTodoList] = useState([])
   const [postList, setPostList] = useState([])
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 1,
+  })
+  const [filters, setFilters] = useState({
+    _page: 1,
+    _limit: 10,
+  })
 
   useEffect(() => {
     async function fetchPostList() {
       try {
-        const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1'
+        const paramsString = queryString.stringify(filters)
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`
         const response = await fetch(requestUrl)
         const responseJSON = await response.json()
-        const { data } = responseJSON
+        const { data, pagination } = responseJSON
         setPostList(data)
+        setPagination(pagination)
       } catch (err) {
         console.log(err.message)
       }
     }
 
     fetchPostList()
-  }, [])
+  }, [filters])
+
+  function handlePageChange(newPage) {
+    setFilters({
+      ...filters,
+      _page: newPage,
+    })
+  }
 
   function handleTodoClick(todo) {
     const index = todoList.findIndex((x) => x.id === todo.id)
@@ -53,6 +73,7 @@ function App() {
       <TodoForm onSubmit={handleTodoFormSubmit} />
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
       <PostList posts={postList} />
+      <Pagination pagination={pagination} onPageChange={handlePageChange} />
     </div>
   )
 }
